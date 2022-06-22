12639889471bc2e005637711c179090adbe1e2f5
Nathaniel Tadesse <tadesse.nate13@gmail.com>
2022-06-22 15:04:25 -0400

    feat: add cross compilation support for C in C++ main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <cstring> 
 
 #include "statement_list.hpp"
+#include "entry.h"
+#include <stdio.h>
 
 // Author: Nathan Tadesse
 // Date: 06-19-22
@@ -17,7 +19,7 @@
  *
  * @argc: number of command line arguments
  * @argv: the strings representing command line arguments. Expects a filename that points to a
- *        space delimted file of the representation in description, and an output file specifier for the .dat file
+ *        csv file of the representation in description, and an output file specifier for the .dat file
  *
  *  Description:
  *          Given an input file that references a file containing a Capital One Bank Statement
@@ -38,6 +40,16 @@
  */
 
 
+/*
+ * Macro below represents the maximum line length of the input file.
+ * On *nix systems the maximum line length of a file can be found by
+ * running 'wc -L inputfile'. Replace that number+1(include space for null termination char)
+ * with the MACRO value below.
+ *
+ */
+#define CSV_STATEMENT_LENGTH 130
+#define TEMP_STATEMENT_LENGTH 18 
+
 int main(int argc, char *argv[]) {
 
   if (argc != 3) {
@@ -45,26 +57,59 @@ int main(int argc, char *argv[]) {
     return -1; 
   }
 
-  std::ifstream ifs(argv[1], std::ifstream::in); 
-  std::ofstream ofs(argv[2], std::ofstream::out); 
-  if (!ifs.is_open() || !ofs.is_open())  {
-    std::cout << "Failed to open input/output file. Please check directory permissions\n"; 
+  /**** START OF C Function Calls ****/ 
+
+  FILE *p_inputfile = fopen(argv[1], "r");  
+  const char *temp_filename = "temp.out"; 
+  FILE *p_tmpfile = fopen(temp_filename, "w"); 
+
+  if (p_inputfile == NULL || p_tmpfile == NULL) {
+    perror("Failed to open .csv file"); 
     return -1; 
   }
 
-  const unsigned statementLength = 18; 
-  StatementList sl(statementLength, std::regex{R"((\S{8}) (-?\d+) (-?\d+))"} );
+  char csv_statement[CSV_STATEMENT_LENGTH];
+  size_t number_entries = 0;
+  while(fgets(csv_statement, CSV_STATEMENT_LENGTH, p_inputfile)) {
+    number_entries++;
+  }
+  fseek(p_inputfile, 0, SEEK_SET);
 
-  char statement[statementLength]; 
-  while(ifs.getline(statement, statementLength)) { /* For every statement */ 
-    sl.addStatement(statement); 
+  struct Entry *pentries[number_entries];
+  for (size_t j = 0; fgets(csv_statement, CSV_STATEMENT_LENGTH, p_inputfile); j++) {
+    // dynamic memory returned
+    pentries[j] = create_entry(csv_statement);
+    if (pentries[j]) {
+      fprintf(p_tmpfile, "%s %d %d\n", get_date(pentries[j]), get_amount(pentries[j]), get_balance(pentries[j]));
+    }
+    else {
+      perror("Failed to parse csv_statement.\n");
+    }
+  } /* Get next statement */
+
+  /* No more operations on entries past here */
+  free_entries(number_entries, pentries);
+  /* End of file operations */
+
+  fclose(p_inputfile);
+  fclose(p_tmpfile); 
+  /**** END OF C Function Calls ****/ 
+
+  std::ifstream ifs(temp_filename, std::ifstream::in); 
+  std::ofstream ofs(argv[2], std::ofstream::out); 
+  if (!ifs.is_open() || !ofs.is_open())  {
+    std::cout << "Failed to open files. Please check directory permissions\n"; 
+    return -1; 
+  }
+
+  StatementList sl(TEMP_STATEMENT_LENGTH, std::regex{R"((\S{8}) (-?\d+) (-?\d+))"} );
+  char temp_statement[TEMP_STATEMENT_LENGTH]; 
+  while(ifs.getline(temp_statement, TEMP_STATEMENT_LENGTH)) { /* For every statement */ 
+    sl.addStatement(temp_statement); 
  
   } /* Finished reading file */ 
   
-  
-
   ofs << sl; 
-  
   std::cout << "Max deposit: " << sl.getMaxDeposit() << " happened on " << sl.getMaxDepositDate() << '\n';
   std::cout << "Max withdrawal: " << sl.getMaxWithdrawal() << " happened on " << sl.getMaxWithdrawalDate() << '\n';
   std::cout << "Max balance: " << sl.getMaxBalance() << " happened on " << sl.getMaxBalanceDate() << '\n';
