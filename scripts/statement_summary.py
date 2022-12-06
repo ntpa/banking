6d1ff189571672ffd25c6ec576518ea82e05ba66
Nathaniel Tadesse <tadesse.nate13@gmail.com>
2022-12-06 14:44:29 -0500

    reduce imports from decimal

--- scripts/statement_summary.py
+++ scripts/statement_summary.py
@@ -3,7 +3,7 @@
 # Date: December 6, 2022
 # Description: Summary of statement list, 
 #           Assumes: 
-#               - No duplicate statements 
+#               - No duplicate transactions 
 #               - Proper date format(YYYY-MM-DD)
 #               - No CSV Header
 
@@ -11,29 +11,27 @@
 import sys
 import csv
 from datetime import date
-import decimal
-
+from decimal import Decimal
 # Monetary calculations. Inaccurate, but okay for simple use case in the program
 def remove_exponent(d):
-    return d.quantize(decimal.Decimal(1)) if d == d.to_integral() else d.normalize()
-TWOPLACES = decimal.Decimal(10) ** -2       # same as Decimal('0.01')
+    return quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
+
+TWOPLACES = Decimal(10) ** -2       # same as Decimal('0.01')
 
 if (len(sys.argv) < 2):
     print("Usage: python statement_summary.py <statement.csv>")
     exit()
 
-total_statements = 0
-start_date = date.max
-end_date = date.min
-number_deposits = 0
-number_withdrawals = 0
-total_deposit_amount = decimal.Decimal(0)
-total_withdrawal_amount = decimal.Decimal(0)
-
-with open (sys.argv[1], 'r', encoding="utf-8") as csvfile:
+with open (sys.argv[1], 'r', encoding="utf-8") as csvfile
     csv_reader = csv.reader(csvfile, delimiter=',')
     
-
+    total_transactions = 0
+    start_date = date.max
+    end_date = date.min
+    number_deposits = 0
+    number_withdrawals = 0
+    total_deposit_amount = Decimal(0)
+    total_withdrawal_amount = Decimal(0)
 
     for row in csv_reader: 
         # Date Summary
@@ -41,21 +39,22 @@ with open (sys.argv[1], 'r', encoding="utf-8") as csvfile:
         start_date = statement_date if start_date > statement_date else start_date
         end_date = statement_date if end_date < statement_date else end_date
         # Deposit/Withdrawal Summary
-        if (decimal.Decimal(row[2]) > 0):
+        if (Decimal(row[2]) > 0):
             number_deposits += 1
-            total_deposit_amount += decimal.Decimal(row[2])
+            total_deposit_amount += Decimal(row[2])
         else: 
             number_withdrawals += 1 
-            total_withdrawal_amount += decimal.Decimal(row[2])
+            total_withdrawal_amount += Decimal(row[2])
 
-        total_statements += 1
+        total_transactions += 1
+
         
-print("\n{} statements from {} to {}".format(total_statements, start_date, end_date))
+print("\nTotal of {} transactions from {} to {}".format(total_transactions, start_date, end_date))
 print("Breakdown:")
 
 print(f'\t{number_deposits} deposits for a total of {"{:,}".format(remove_exponent(total_deposit_amount))} USD',
-    f' ({decimal.Decimal(total_deposit_amount/number_deposits).quantize(TWOPLACES)} USD per deposit)')
+    f' ({Decimal(total_deposit_amount/number_deposits).quantize(TWOPLACES)} USD per deposit)')
 print(f'\t{number_withdrawals} for a total of {"{:,}".format(remove_exponent(total_withdrawal_amount))} USD',
-    f' ({decimal.Decimal(total_withdrawal_amount/number_withdrawals).quantize(TWOPLACES)} USD per withdrawal)')
+    f' ({Decimal(total_withdrawal_amount/number_withdrawals).quantize(TWOPLACES)} USD per withdrawal)')
 
 
