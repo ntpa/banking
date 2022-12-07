ce5d502c790bc35c2e511ce7199d3963feb83dc4
Nathaniel Tadesse <tadesse.nate13@gmail.com>
2022-12-06 20:15:00 -0500

    code refactor

--- scripts/merge_statements.py
+++ scripts/merge_statements.py
@@ -10,7 +10,6 @@
 #                       (Capital One Statements CSV date currently comes standard
 #                       with MM/DD/YY format. However, this does not play nice with 
 #                       databases.) 
-# 
 #                   2. Remove any duplicate statements
 #                   3. Append the two statement lists
 #                   4. Print to STDOUT 
@@ -23,14 +22,18 @@
 import sys
 import csv
 
-# Format a given date to format YYYY-DD-MM. 
-# Function assumes that dates come in the format(s): 
-#   - YYYY-DD-MM
-#   - MM/DD/YY (Capital One Statements at this point in time come in this format).
+''''
+    name: formate_date(transaction_date)
+    args: transaction_date - string 
+    description: 
+        Format a given transaction date to YYYY-DD-MM formatted string. 
+        Function assumes that date passed in the format: 
+        YYYY-DD-MM or MM/DD/YY (Capital One Statement's transaction date form)
+'''
 def format_date(transaction_date): 
-    if (len(transaction_date.split('/')) == 3): # MM/DD/YY
+    if (len(transaction_date.split('/')) == 3): # MM/DD/YY format found
         month, day, year = transaction_date.split('/')
-        return '20{}-{}-{}'.format(year, month, day)
+        return f"20{year}-{month}-{day}"
     else:
         return transaction_date # date already in correct format. see function call assumptions
     
@@ -43,24 +46,27 @@ if (len(sys.argv) < 3):
     exit()
 
 # Set chosen due to unique property. Important to take the 
-# entire line as set value, because multiple transactions can happen on 
-# the same date, while all being unique transactions. 
+# entire transaction as set value, because multiple transactions can happen on 
+# the same date while all being unique transactions. 
 transactions = set()
 
 # read statement 1 
 with open (sys.argv[1], 'r', encoding="utf-8") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader) # skip header
-    for row in csv_reader: 
-        transactions.add("{},{},{},{},{},{}".format(row[0], format_date(row[1]),
-                                                  row[2], row[3], row[4], row[5]))
+    for row in csv_reader:  
+        # Account Number,Transaction Date,Transaction Amount,Transaction Type,Transaction Description,Balance 
+        transaction = "{},{},{},{},{},{}".format(row[0], format_date(row[1]),
+                                                  row[2], row[3], row[4], row[5])
+        transactions.add(transaction)
 # read statement 2 
 with open (sys.argv[2], 'r', encoding="utf-8") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader) 
     for row in csv_reader: 
-        transactions.add('{},{},{},{},{},{}'.format(row[0], format_date(row[1]),
-                                                  row[2], row[3], row[4], row[5]))
+        transaction = "{},{},{},{},{},{}".format(row[0], format_date(row[1]),
+                                                  row[2], row[3], row[4], row[5])
+        transactions.add(transaction)
 
 for transaction in transactions:
     print(transaction)
--- scripts/statement_summary.py
+++ scripts/statement_summary.py
@@ -12,17 +12,24 @@ import sys
 import csv
 from datetime import date
 from decimal import Decimal
+
+TWOPLACES = Decimal(10) ** -2       # same as Decimal('0.01')
+
 # Monetary calculations. Inaccurate, but okay for simple use case in the program
 def remove_exponent(d):
     return quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
 
-TWOPLACES = Decimal(10) ** -2       # same as Decimal('0.01')
+def is_deposit(amount):
+    return True if Decimal(amount) > 0 else False
+    
+    
+
 
 if (len(sys.argv) < 2):
     print("Usage: python statement_summary.py <statement.csv>")
     exit()
 
-with open (sys.argv[1], 'r', encoding="utf-8") as csvfile
+with open (sys.argv[1], 'r', encoding="utf-8") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     
     total_transactions = 0
@@ -39,7 +46,7 @@ with open (sys.argv[1], 'r', encoding="utf-8") as csvfile
         start_date = statement_date if start_date > statement_date else start_date
         end_date = statement_date if end_date < statement_date else end_date
         # Deposit/Withdrawal Summary
-        if (Decimal(row[2]) > 0):
+        if (is_deposit(row[2])):
             number_deposits += 1
             total_deposit_amount += Decimal(row[2])
         else: 
@@ -49,11 +56,12 @@ with open (sys.argv[1], 'r', encoding="utf-8") as csvfile
         total_transactions += 1
 
         
-print("\nTotal of {} transactions from {} to {}".format(total_transactions, start_date, end_date))
+print("\nTotal of {total_transactions} transactions from {start_date} to {end_date}")
 print("Breakdown:")
 
 print(f'\t{number_deposits} deposits for a total of {"{:,}".format(remove_exponent(total_deposit_amount))} USD',
     f' ({Decimal(total_deposit_amount/number_deposits).quantize(TWOPLACES)} USD per deposit)')
+    
 print(f'\t{number_withdrawals} for a total of {"{:,}".format(remove_exponent(total_withdrawal_amount))} USD',
     f' ({Decimal(total_withdrawal_amount/number_withdrawals).quantize(TWOPLACES)} USD per withdrawal)')
 
